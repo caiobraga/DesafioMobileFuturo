f6a866ffd0368a063e9136fd3e75188c0ceea7de
caiobraga <caiotagb@gmail.com>
2022-03-05 10:24:45 -0300

    adição entity pessoa

---
+++ __tests__/domain/entitys/pessoa-test.ts
@@ -0,0 +1,42 @@
+/**
+ * @format
+ */
+
+ import 'react-native';
+
+import { Pessoa } from '../../../src/domain/entitys/pessoa';
+
+
+ test('teste pessoa entity declaration', async () => {
+      
+
+      let pessoa = new Pessoa("caio", "27988860342", "caiotagb@gmail.com" );
+
+      expect(pessoa.nome).toBe("caio");
+      expect(pessoa.telefone).toBe("27988860342");
+      expect(pessoa.email).toBe("caiotagb@gmail.com");
+ });
+
+ test('teste função isEmailValid', async () => {
+      
+
+     let pessoa = new Pessoa("caio", "27988860342", "caiotagb@gmail.com" );
+
+     expect(pessoa.isEmailValid).toBeTruthy();
+ 
+});
+
+test('teste função isTelefoneValid', async () => {
+      
+
+     let pessoa = new Pessoa("caio", "27988860342", "caiotagb@gmail.com" );
+     let pessoa2 = new Pessoa("joao", "(27) 988860342", "caiotagb@gmail.com" );
+     let pessoa3 = new Pessoa("ricardo", "27 988860342", "caiotagb@gmail.com" );
+     let pessoa4 = new Pessoa("aumeida", "27 98886-0342", "caiotagb@gmail.com" );
+
+     expect(pessoa.isTelefoneValid).toBeTruthy();
+     expect(pessoa2.isTelefoneValid).toBeTruthy();
+     expect(pessoa3.isTelefoneValid).toBeTruthy();
+     expect(pessoa4.isTelefoneValid).toBeTruthy();
+});
+ 
---
+++ src/domain/entitys/pessoa.ts
@@ -0,0 +1,35 @@
+
+export class Pessoa {
+    nome: string;
+    telefone: string;
+    email: string;
+    
+    constructor(nome: string, telefone: string, email: string) {
+		this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+	}
+
+    isEmailValid<bool>(){  
+        var regex = new RegExp('/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i'); 
+
+        /*
+        foo.bar@gmail.com       true
+        foo.bar@gmail.com.br    true
+        foo.bar@gmail.com.br.br false
+        foo.bar@gmail.          false
+        foo.bar@gmailcom        false
+        foo.bargmail.com        false
+        @gmail.com              false
+        */
+
+        return regex.test(this.email);
+    }
+
+    isTelefoneValid<bool>(){
+        var regex = new RegExp('^\(?[1-9]{2}\)? ?(?:[2-8]|9[1-9])[0-9]{3}\-?[0-9]{4}$');  //xx xxxxx-xxxx - parenteses e ifens opcionais
+
+        return regex.test(this.telefone);
+    }
+}
+
