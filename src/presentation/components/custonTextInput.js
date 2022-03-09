ccbd77ff99458f3054c903eb7082a0e523f8ba8d
caiobraga <caiotagb@gmail.com>
2022-03-09 16:31:23 -0300

    alteração de estilo e refatoração

--- src/presentation/components/custonTextInput.js
+++ src/presentation/components/custonTextInput.js
@@ -1,30 +1,48 @@
 import React from 'react';
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { TextInput } from 'react-native-paper';
 
-const CustonTextInput = ({label, onChangeText, text, hasError, Error}) => {
+
+const CustonTextInput = ({label, onChangeText, text, error, placeholder, description, title, color, type}) => {
+
+  const typografy = new Typografy(props.type);
+
   const styles = StyleSheet.create({
-    input: {
-      height: 40,
-      margin: 12,
-      borderWidth: 1,
-      padding: 10,
+    TextInput: {
+    fontFamily: typografy.fontFamily,
+    fontStyle: typografy.fontStyle,
+    fontWeight: typografy.fontWeight,
+    fontSize: typografy.fontSize,
+    lineHeight: typografy.lineHeight,
+    color: typografy.color,
     },
   });
-    return (
-      !hasError?
-      <TextInput
-      label={label}
-      value={text}
-      onChangeText={(text)=>{onChangeText(text)}}
-    />
-      :
-      <TextInput
 
+    return (
+     
+      <View>
+      <Text style={{
+        color: color
+      }}>{title? title: ""}</Text>
+
+      <TextInput
+      error={error}
       label={label}
+      placeholder={placeholder}
       value={text}
       onChangeText={text => onChangeText(text)}
-    />
+      underlineColor={color}
+      style={
+        styles.TextInput
+      }
+    /> 
+      <Text style={{
+        color: color
+      }}>{ description? description : "" }</Text>
+   
+    </View>
+     
+
     );
   }
 
